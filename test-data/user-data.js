61f5615411c9e40454e02066a4a39f6d58a3c96c
Egor Sorokin <e.sorokin@pulkovo-service.ru>
2023-08-03 15:43:40 +0300

    add-scripts-for-success-and-fail

--- test-data/user-data.js
+++ test-data/user-data.js
@@ -1,6 +1,6 @@
 const users = {
     username: {
-        standart: 'standard_user',
+        // standart: 'standard_user',  // ? 'standard_user' : 'problem_user'
         locked: 'locked_out_user',
         problem: 'problem_user',
         wrong: 'user_standart',
@@ -10,6 +10,8 @@ const users = {
         wrong: 'sauce_secret',
     },
 };
+
+users.username.standart = process.env.USER === 'success' ? 'standard_user' : 'problem_user';
 
 const checkout = {
     firstname: 'Georgiy',
